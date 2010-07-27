1f2af39b79c8d78d4ca92eef9dee370e9a989b59
juangarat <juangarat@820dba90-e68b-11de-9229-05bbbb8527c3>
2010-07-27 04:51:40 +0000

    Se agregan las clases Escalafon.cs y EscalafonEmpleado.cs.

---
+++ trunk/Codigo/Dominio/EscalafonEmpleado.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logica
+{
+    class EscalafonEmpleado
+    {
+        public HorarioXDia Horario { set; get; }
+        public int CantidadHsLlamadaAntesHoraInicio { set; get; }
+        public enum ACargo { Empresa, Cliente }
+
+        public ACargo ACargoDe;
+
+        public Empleado Funcionario { set; get; }
+        public Escalafon Planificacion { set; get; }
+
+
+        public EscalafonEmpleado()
+        {
+
+        }
+
+        public EscalafonEmpleado(HorarioXDia horario, int cantHsLlamadaAntesHoraInicio, ACargo aCargo, Empleado empleado, Escalafon escalafon)
+        {
+            this.Horario = horario;
+            this.CantidadHsLlamadaAntesHoraInicio = cantHsLlamadaAntesHoraInicio;
+            this.ACargoDe = aCargo;
+            this.Funcionario = empleado;
+            this.Planificacion = Planificacion;
+        }
+
+        public EscalafonEmpleado(string dia, string horaInicio, string horaFin, int cantHsLlamadaAntesHoraInicio, ACargo aCargo, Empleado empleado, Escalafon escalafon)
+        {
+            Horario = new HorarioXDia(dia,horaInicio,horaFin);
+            this.CantidadHsLlamadaAntesHoraInicio = cantHsLlamadaAntesHoraInicio;
+            this.ACargoDe = aCargo;
+            this.Funcionario = empleado;
+            this.Planificacion = Planificacion;
+        }
+
+
+
+    }
+}
