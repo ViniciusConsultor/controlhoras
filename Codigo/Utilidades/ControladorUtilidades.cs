d23888e32e6192f9b3f7fa413c090d55010cc144
juangarat <juangarat@820dba90-e68b-11de-9229-05bbbb8527c3>
2011-07-05 05:38:18 +0000

    Version 0.4.4.6 y 0.4.4.7. Issue 34.

--- Codigo/Utilidades/ControladorUtilidades.cs
+++ Codigo/Utilidades/ControladorUtilidades.cs
@@ -9,6 +9,8 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Configuration;
+using System.Runtime.InteropServices;
+using System.Globalization;
 
 namespace Utilidades
 {
@@ -86,7 +88,7 @@ namespace Utilidades
             }
         }
 
-        public static bool exportToExcel(DataGridView dgv, string fileName)
+        public static bool exportToExcel_old(DataGridView dgv, string fileName)
         {
             ExcelXmlWorkbook libro = new ExcelXmlWorkbook();
 
@@ -126,7 +128,7 @@ namespace Utilidades
             int fila = 0;
             int columna = 0;
             double[] maxWithPerColumn;
-
+            TimeSpan h;
             maxWithPerColumn = new double[dgv.Columns.Count];
             DisplayFormatType cellType;
             while (fila < dgv.Rows.Count)
@@ -137,9 +139,17 @@ namespace Utilidades
                     
                     if (dgv[columna, fila].ValueType == typeof(DateTime))
                     {
-                        cellType = DisplayFormatType.ShortDate;
+                        cellType = DisplayFormatType.GeneralDate;
                         if (dgv[columna,fila].Value.ToString() != "")
                             hoja[columna, fila+1].Value = ((DateTime)dgv[columna,fila].Value).ToShortDateString();
+                    }
+                    else if (dgv[columna, fila].ValueType == typeof(TimeSpan))
+                    {
+                        cellType = DisplayFormatType.Time;
+                        h = (TimeSpan)dgv[columna, fila].Value;
+                        if (dgv[columna, fila].Value.ToString() != "")
+                            hoja[columna, fila + 1].Value = System.Math.Abs(System.Math.Truncate(h.TotalHours)).ToString()  + ":" + System.Math.Abs(h.Minutes).ToString();          
+                                //((TimeSpan)dgv[columna, fila].Value).ToString();
                     }
                     else if (dgv[columna, fila].ValueType == typeof(Boolean))
                     {
@@ -190,188 +200,170 @@ namespace Utilidades
             return retval;
         }
 
-        //public static bool exportToExcel_COM(DataGridView dgv, string fileName)
-        //{
 
-        //    object readOnly = true;
-        //    object falso = false;
-        //    DateTime auxdt;
-        //    object missing = null;
+        public static bool exportToExcel(DataGridView dgv, string fileName, string titulo, string userName)
+        {
+            // FUENTE: http://msdn.microsoft.com/en-us/library/microsoft.office.interop.excel(v=office.11).aspx
+            object readOnly = true;
 
-        //    Excel.Application ExApp;
-        //    Excel._Workbook oWBook;
-        //    Excel._Worksheet oSheet;
-        //    try
-        //    {
-        //        // Iniciar Excel y obtener el objeto Aplicacion.
-        //        ExApp = new Excel.Application();
+            Excel.Application ExApp = null;
+            Excel._Workbook oWBook = null;
+            Excel._Worksheet oSheet = null;
+            Excel.Range oRng = null;
+            CultureInfo CultOriginal = System.Threading.Thread.CurrentThread.CurrentCulture;
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            try
+            {
+                // Iniciar Excel y obtener el objeto Aplicacion.
+                ExApp = new Excel.Application();
 
-        //        //Get a new workbook.
-        //        //oWB = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
-        //        oWBook = ExApp.Workbooks.Open(fileName, missing, readOnly, missing, missing, missing, missing, missing, missing, missing, missing, missing, missing, missing, missing);
-        //        oSheet = (Excel._Worksheet)oWBook.ActiveSheet;
+                //Get a new workbook.
+                //oWB = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
+                object misValue = System.Reflection.Missing.Value;
 
-        //        oWBook.Author = "Trust International S.A."; 
-                         
-        //        oSheet.Name = "Exportacion";
-        //        //hoja.FreezeTopRows = 3;
-        //        //hoja.DisplayFormat = DisplayFormatType.None;
-        //        //hoja.PrintOptions.Orientation = PageOrientation.Landscape;
-        //        //hoja.PrintOptions.SetMargins(0.5, 0.4, 0.5, 0.4);
+
+                ExApp = new Excel.ApplicationClass();
+                oWBook = ExApp.Workbooks.Add(misValue);
+                oSheet = (Excel.Worksheet)oWBook.Worksheets.get_Item(1);
                 
-        //        XmlStyle estiloCeldaNombreColumnas = new XmlStyle();
-        //        estiloCeldaNombreColumnas.Font.Bold = true;
-        //        estiloCeldaNombreColumnas.Alignment.Horizontal = Yogesh.ExcelXml.HorizontalAlignment.Center;
-        //        estiloCeldaNombreColumnas.Border.LineStyle = Borderline.Continuous;
-        //        estiloCeldaNombreColumnas.Border.Weight = 2;
-        //        estiloCeldaNombreColumnas.Border.Sides = BorderSides.All;
+                oWBook.Author = "Trust International S.A.";
+                oSheet.Name = "Exportacion";
 
+                // Cargamos el titulo, linea 1
+                oRng = oSheet.get_Range(oSheet.Cells[1, 1], oSheet.Cells[1, dgv.Columns.Count]);
+                oRng.Cells.MergeCells = true;
+                oRng.Cells.Value2 = titulo;
+                oRng.Cells.HorizontalAlignment = Excel.XlHAlign.xlHAlignLeft;
+                oRng.Cells.Font.FontStyle = FontStyle.Bold; 
+                oRng.Cells.Font.Size = 14;
 
+                // UserName
+                oRng = oSheet.get_Range(oSheet.Cells[2, 1], oSheet.Cells[2, 4]);
+                oRng.Interior.Color = ColorTranslator.ToOle(Color.Beige);
+                oRng.Cells.Font.Bold = true;
+                oRng.Cells.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                oRng.Cells.MergeCells = true;
+                oRng.Cells.Value2 = "Usuario: " + userName;
 
-        //        // Primero cargo todas las columnas permitiendo hacerle algun formato diferente.
-        //        foreach (DataGridViewColumn col in dgv.Columns)
-        //        {
-        //            oSheet.Cells[col.Index, 0] = col.Name;
-        //            //hoja[col.Index, 0].Style = estiloCeldaNombreColumnas;
-        //        }
-
+                // Mostramos el usuario y la fecha de generacion, linea 1
+                oRng = oSheet.get_Range(oSheet.Cells[2, 7], oSheet.Cells[2, 10]);
+                oRng.Cells.MergeCells = true;
+                oRng.Interior.Color = ColorTranslator.ToOle(Color.Beige);
+                oRng.Cells.Font.Bold = true;
+                oRng.Cells.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                oRng.Cells.Value2 = "Fecha: "+DateTime.Now.ToString("dd/MM/yyyy HH:mm");
                 
-        //        // Mes
-        //        oSheet.Cells[1, 5] = dtpMesFacturacion.Value.Month + "/" + dtpMesFacturacion.Value.Year;
-        //        oSheet.Cells[2, 3] = facturacion.NroCliente + "/" + datos.getNombreCliente(facturacion.NroCliente);
-        //        // Nombre
-        //        string nomServicio = nombreArchivo.Substring(nombreArchivo.LastIndexOf('-') + 1);
-        //        oSheet.Cells[3, 3] = nomServicio;
+                int fila_offset = 4;
 
-        //        int i = 0;
-        //        foreach (DataDiaFacturacion ddf in facturacion.ListaDiaFacturacion)
-        //        {
-        //            oSheet.Cells[6 + i, 2] = ddf.Dia.ToString(@"dd/MM/yyyy");
-        //            oSheet.Cells[6 + i, 3] = String.Format("{0}:{1}", ddf.HsComunes.Days * 24 + ddf.HsComunes.Hours, ddf.HsComunes.Minutes);
-        //            oSheet.Cells[6 + i, 4] = String.Format("{0}:{1}", ddf.HsExtras.Days * 24 + ddf.HsExtras.Hours, ddf.HsExtras.Minutes); ;
-        //            if (esFeriado(ddf.Dia))
-        //            {
-        //                Excel.Range MyRange;
-        //                MyRange = (Excel.Range)oSheet.get_Range(
-        //                    oSheet.Cells[6 + i, 2], oSheet.Cells[6 + i, 4]);
-        //                MyRange.Interior.Color = System.Drawing.Color.YellowGreen.ToArgb();
-        //                tieneFeriados = true;
-        //            }
-        //            i++;
-        //        }
+                // Primero cargo todas las columnas permitiendo hacerle algun formato diferente.
+                foreach (DataGridViewColumn col in dgv.Columns)
+                {
+                    oSheet.Cells[fila_offset, col.Index + 1] = col.Name;
+                }
 
-        //        oSheet.Cells[31 + 6, 3] = String.Format("{0}:{1}:{2}", facturacion.TotalHsComunes.Days * 24 + facturacion.TotalHsComunes.Hours, facturacion.TotalHsComunes.Minutes, facturacion.TotalHsComunes.Seconds);
-        //        oSheet.Cells[31 + 6, 4] = String.Format("{0}:{1}:{2}", facturacion.TotalHsExtras.Days * 24 + facturacion.TotalHsExtras.Hours, facturacion.TotalHsExtras.Minutes, facturacion.TotalHsExtras.Seconds);
-
-        //        if (tieneFeriados)
-        //        {
-        //            oSheet.Cells[40, 3] = "Dias Feriados";
-        //            oSheet.get_Range(oSheet.Cells[40, 2], oSheet.Cells[40, 2]).Interior.Color = System.Drawing.Color.YellowGreen.ToArgb();
-        //        }
-
-        //        //oWBook.PrintPreview(falso);
-        //        object a = Path.Combine(outputDirectory, nombreArchivo + ".xls");
-        //        if (!abrirExcel)
-        //        {
-        //            ExApp.Visible = false;
-        //            oWBook.SaveAs(a, missing, missing, missing, missing, missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Microsoft.Office.Interop.Excel.XlSaveConflictResolution.xlLocalSessionChanges, missing, missing, missing, true);
-        //            ExApp.Quit();
-        //        }
-        //        else
-        //            ExApp.Visible = true;
-        //    }
-        //    catch (Exception theException)
-        //    {
-        //        String errorMessage;
-        //        errorMessage = "Error: ";
-        //        errorMessage = String.Concat(errorMessage, theException.Message);
-        //        errorMessage = String.Concat(errorMessage, " Line: ");
-        //        errorMessage = String.Concat(errorMessage, theException.Source);
-        //        throw theException;
-        //    }
+                oRng = oSheet.get_Range(oSheet.Cells[fila_offset, 1], oSheet.Cells[fila_offset, dgv.Columns.Count]);
+                oRng.Font.Bold = true;
+                //oRng.Style = 
+                oRng.BorderAround(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlMedium, Excel.XlColorIndex.xlColorIndexAutomatic, System.Type.Missing);
 
 
+                int fila = 0;
+                int columna = 0;
+                fila_offset++;
+                // Mes
+                columna = 1;
+               
+                while (columna <= dgv.Columns.Count)
+                {
+                    fila = 0;
+                    while (fila < dgv.Rows.Count)
+                    {    
+                        if (dgv[columna - 1, fila].ValueType == typeof(TimeSpan) || dgv[columna-1,fila].Value.ToString().Contains(':'))
+                            oSheet.Cells[fila + fila_offset, columna] = dgv[columna - 1, fila].Value.ToString();
+                        else
+                            oSheet.Cells[fila + fila_offset, columna] = dgv[columna - 1, fila].Value;
+                        fila++;
+                    }
 
-        //    ExcelXmlWorkbook libro = new ExcelXmlWorkbook();
+                    oRng = oSheet.get_Range(oSheet.Cells[fila_offset, columna], oSheet.Cells[fila + fila_offset, columna]);
+                    if (dgv[columna - 1, 0].ValueType == typeof(DateTime))
+                    {
+                        oRng.NumberFormat = "DD/MM/YYYY";
+                        oRng.Cells.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
 
-            
+                    }
+                    else if (dgv[columna - 1, 0].ValueType == typeof(TimeSpan) || dgv[columna - 1, 0].Value.ToString().Contains(':'))
+                    {
 
-            
+                        oRng.NumberFormat = "[h]:mm";
+                        oRng.Cells.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                    }
+                    else if (dgv[columna - 1, 0].ValueType == typeof(Boolean))
+                    {
+                        oRng.NumberFormat = "Sí;Sí;No";
+                        oRng.Cells.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
 
-        //    XmlStyle estiloCeldaNormal = new XmlStyle();
-        //    estiloCeldaNormal.Font.Bold = false;
-        //    estiloCeldaNormal.Alignment.Horizontal = Yogesh.ExcelXml.HorizontalAlignment.Left;
-        //    estiloCeldaNormal.Border.LineStyle = Borderline.Continuous;
-        //    estiloCeldaNormal.Border.Weight = 1;
-        //    estiloCeldaNormal.Border.Sides = BorderSides.All;
-        //    //estiloCeldaNormal.DisplayFormat = DisplayFormatType.Text;
-            
-        //    int fila = 0;
-        //    int columna = 0;
-        //    double[] maxWithPerColumn;
+                    }
+                    else if (dgv[columna - 1, 0].ValueType == typeof(Int64) || dgv[columna - 1, 0].ValueType == typeof(int) || dgv[columna - 1, 0].ValueType == typeof(uint) || dgv[columna - 1, 0].ValueType == typeof(byte) || dgv[columna - 1, 0].ValueType == typeof(sbyte))
+                    {
+                        oRng.Cells.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                        oRng.NumberFormat = "0";
+                    }
+                    else
+                    {
+                        // String
+                        oRng.NumberFormat = "@";
+                        oRng.Cells.HorizontalAlignment = Excel.XlHAlign.xlHAlignLeft;
 
-        //    maxWithPerColumn = new double[dgv.Columns.Count];
-        //    DisplayFormatType cellType;
-        //    while (fila < dgv.Rows.Count)
-        //    {
-        //        columna = 0;
-        //        while (columna < dgv.Columns.Count)
-        //        {
-                    
-        //            if (dgv[columna, fila].ValueType == typeof(DateTime))
-        //            {
-        //                cellType = DisplayFormatType.ShortDate;
-        //                if (dgv[columna,fila].Value.ToString() != "")
-        //                    hoja[columna, fila+1].Value = ((DateTime)dgv[columna,fila].Value).ToShortDateString();
-        //            }
-        //            else if (dgv[columna, fila].ValueType == typeof(Boolean))
-        //            {
-        //                cellType = DisplayFormatType.Custom;
-        //                if (dgv[columna, fila].Value.ToString() != "")
-        //                    hoja[columna, fila+1].Value = ((Boolean)dgv[columna, fila].Value);
-        //            }
-        //            else if (dgv[columna, fila].ValueType == typeof(int) || dgv[columna, fila].ValueType == typeof(uint) || dgv[columna, fila].ValueType == typeof(byte) || dgv[columna, fila].ValueType == typeof(sbyte))
-        //            {
-        //                cellType = DisplayFormatType.Standard;
-        //                if (dgv[columna, fila].Value.ToString() != "")
-        //                    hoja[columna, fila + 1].Value = dgv[columna, fila].Value;
-        //            }
-        //            else
-        //            {
-        //                // String
-        //                String str = dgv[columna, fila].Value.ToString();//.Replace("\n",Environment.NewLine);
-        //                cellType = DisplayFormatType.Text;
-        //                hoja[columna, fila + 1].Value = str;
-                        
-        //            }
-                    
-        //            if (dgv[columna, fila].Value.ToString().Length > maxWithPerColumn[columna])
-        //            {
-        //                maxWithPerColumn[columna] = (double)dgv[columna, fila].Value.ToString().Length;
-        //            }
-        //            //hoja[columna, fila].ContentType = ContentType.Formula;
-        //            estiloCeldaNormal.DisplayFormat = cellType;
-        //            hoja[columna, fila + 1].Style = estiloCeldaNormal;
-        //            columna++;
+                    }
 
-        //        }
-        //        fila++;
-        //    }
-        //    columna = 0;
-        //    while (columna < dgv.Columns.Count)
-        //    {
-        //        hoja.Columns(columna).Width = dgv.Columns[columna].Width;
-        //        columna++;
-        //    }
-            
-        //    // no extension is added if not present
+                    oRng.EntireColumn.AutoFit();
+                    oRng.BorderAround(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThin, Excel.XlColorIndex.xlColorIndexAutomatic, System.Type.Missing);
+                    Marshal.FinalReleaseComObject(oRng);
+                    columna++;
 
-        //    Stream sw = new FileStream(fileName,FileMode.Create);
-        //    bool retval;
-        //    retval = libro.Export(sw);
-        //    sw.Close();
-        //    return retval;
-        //}
+                }
+
+
+                ExApp.Visible = false;
+                //oWBook.SaveAs(fileName, missing, missing, missing, missing, missing, Excel.XlSaveAsAccessMode.xlExclusive, Excel.XlSaveConflictResolution.xlLocalSessionChanges, missing, missing, missing, true);
+                oWBook.SaveAs(fileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, Excel.XlSaveConflictResolution.xlLocalSessionChanges, misValue, misValue, misValue, misValue);
+                oWBook.Close(true, misValue, misValue);
+                //ExApp.Save(fileName);
+
+                return true;
+            }
+            catch (Exception theException)
+            {
+                String errorMessage;
+                errorMessage = "Error: ";
+                errorMessage = String.Concat(errorMessage, theException.Message);
+                errorMessage = String.Concat(errorMessage, " Line: ");
+                errorMessage = String.Concat(errorMessage, theException.Source);
+                throw theException;
+            }
+            finally
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                if (oRng != null)
+                    Marshal.FinalReleaseComObject(oRng);
+                if (oSheet != null)
+                    Marshal.FinalReleaseComObject(oSheet);
+                if (oWBook != null)
+                    Marshal.FinalReleaseComObject(oWBook);
+                if (ExApp != null)
+                {
+                    ExApp.Quit();
+                    Marshal.FinalReleaseComObject(ExApp);
+                }
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                System.Threading.Thread.CurrentThread.CurrentCulture = CultOriginal;
+            }
+
+
+        }
 
 
 
