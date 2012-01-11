63c0e8fcf301e5352cc15b1dbf5fd21a32ace80c
juangarat <juangarat@820dba90-e68b-11de-9229-05bbbb8527c3>
2012-01-11 18:21:58 +0000

    Se crea clase statica GlobalData para mantener datos globales.

---
+++ Codigo/Utilidades/GlobalData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utilidades
+{
+    public static class GlobalData
+    {
+        private static string m_UserNameLogged = "";
+
+        public static string UserNameLogged
+        {
+            get { return m_UserNameLogged; }
+            set { m_UserNameLogged = value; }
+        }
+    }
+}
