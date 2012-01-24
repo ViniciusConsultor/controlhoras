8049d83d56de20f7cbc813ac326f7aae74cdfdaa
juangarat <juangarat@820dba90-e68b-11de-9229-05bbbb8527c3>
2012-01-24 20:03:58 +0000

    Modificaciones a la BD por issue 101.

--- BasedeDatos/2012-01-11-Modificaciones.sql
+++ BasedeDatos/2012-01-11-Modificaciones.sql
@@ -1,1 +1,7 @@
+/* Version 0.4.4.12
 CREATE TABLE `logevento` (  `Id` bigint(20) NOT NULL AUTO_INCREMENT,  `Fecha` datetime NOT NULL,  `Username` varchar(30) NOT NULL,  `Evento` varchar(100) DEFAULT NULL,  `Descripcion` varchar(1000) NOT NULL,  `PCHostname` varchar(50) NOT NULL,  PRIMARY KEY (`Id`))
+*/
+/* Version 0.4.4.15 */
+CREATE TABLE `listanegraservicio` (  `NumeroCliente` mediumint(8) unsigned NOT NULL,  `NumeroServicio` mediumint(8) unsigned NOT NULL,  `NroEmpleado` mediumint(8) unsigned NOT NULL,  PRIMARY KEY (`NumeroCliente`,`NumeroServicio`,`NroEmpleado`));
+alter table listanegraservicio add foreign key (NumeroCliente,NumeroServicio) references servicios(NumeroCliente, NumeroServicio);
+alter table listanegraservicio add foreign key (NroEmpleado) references empleados(NroEmpleado);
