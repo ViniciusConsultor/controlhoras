7ba0f9c76ccc6dcbd55314ad4871b4da7a50f076
juangarat <juangarat@820dba90-e68b-11de-9229-05bbbb8527c3>
2012-01-24 03:09:39 +0000

    Tabla LogEvento para Release 0.4.4.12

--- trunk/BasedeDatos/2012-01-11-Modificaciones.sql
+++ trunk/BasedeDatos/2012-01-11-Modificaciones.sql
@@ -1,1 +1,1 @@
-create table LogEvento (Id bigint not null auto_increment primary key, Fecha DateTime not null, Username varchar(30) not null, Evento varchar(100), Descripcion varchar(1000) not null);
+CREATE TABLE `logevento` (  `Id` bigint(20) NOT NULL AUTO_INCREMENT,  `Fecha` datetime NOT NULL,  `Username` varchar(30) NOT NULL,  `Evento` varchar(100) DEFAULT NULL,  `Descripcion` varchar(1000) NOT NULL,  `PCHostname` varchar(50) NOT NULL,  PRIMARY KEY (`Id`))
